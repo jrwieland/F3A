f934c2c1a7357b6f12dce704c94ae340aaddf7a5
John Wieland <john.r.wieland@gmail.com>
2021-04-02 13:05:34 -0400

    Upload 2021 AMA Sportman, and inter F21 & P21

---
+++ SOUNDS/en/playlist.lua
@@ -0,0 +1,20 @@
+﻿title="New Playlist"
+playlist={
+{"Vertical 8 - from Inverted with 1/2 Roll on entry (ending at center), Push Loop, Pull Loop, 1/2 Roll on Exit (starting at center) - Exit Inverted","man20",9},
+{"Stall Turn with Two Consecutive 1/4 Rolls Down","man21",3},
+{"Square Loop on Corner with 1/2 Rolls on 2nd and 4th 45 Degree Lines","man22",4},
+{"Figure 9 with Two Consecutive 1/2 Rolls Opposite in the Up Line - Exit Inverted","man23",5},
+{"Knife-Edge Flight with Consecutive 1/4 Roll and 1/2 Roll Opposite, Consecutive 1/2 Roll, 1/4 Roll Opposite","man24",7},
+{"Inverted Split S with Consecutive Two 1/2 Rolls - Exit Inverted","man25",4},
+{"Golf Ball with 1/2 Roll Integrated in Top 180 Degrees","man26",4},
+{"Shark Fin with Consective Two 1/4 Rolls on 45 Degree Down Line ","man27",4},
+{"Double Immelman with 1/2 Roll Entry, Push 1/2 Loop, Consecutive Four 1/8 Rolls, Pull 1/2 Loop, 1/2 Roll Exit - Exit Inverted","man28",9},
+{"Push-Push-Push Humpty Bump with 1/2 Roll on Up Line (Option: 3/4 Roll Up and 1/4 Roll Down) - Exit Inverted","man29",4},
+{"Roll Combination with Consecutive 1/2 Roll, Full Roll, 1/2 Roll in Opposite Directions - Exit Inverted","man30",6},
+{"Top Hat with 1/2 Roll in Up Line, Two 1/2 Turn Spin into a Vertical Downline (Option: 1/4 Roll Up and 1/4 Roll Down after Spin)","man31",4},
+{"Figure Z with Full Roll on 45 Degree Up Line","man32",3},
+{"Comet with Consecutive Two 1/4 Rolls Opposite on 45 Degree Down Line, 1/2 Roll on 45 Degree Up Line","man33",11},
+{"Roll Combination with Consecutive Four 1/4 Rolls","man53",3},
+{"Half Square Loop on Corner with 1/4 Rolls on each 45 Degree Up Line - Exit Inverted","man34",6},
+{"Avalanche - from Inverted pull through a Loop with a Snap Roll at the Bottom - Exit Inverted","man35",5},
+}
