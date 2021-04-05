027e4215644bfadea53f4487f0df9f0cb9f028cc
John Wieland <john.r.wieland@gmail.com>
2021-04-04 21:13:43 -0400

    Updated Call List

--- lists/ama_club/playlist.lua
+++ lists/ama_club/playlist.lua
@@ -1,13 +1,13 @@
 title="AMA Club Class Contest"
 playlist={
 {"Takeoff, Upwind"," "," "," ","man1",5},
-{"Straight Flight Out, Upwind"," "," "," ","man1",5},
-{"Procedure Turn, Turnaround"," "," "," ","man1",5},
-{"Straight Flight Back, Downwind"," "," "," ","man1",5},
-{"Stall Turn without Rolls, Upwind"," "," "," ","man1",5},
-{"Immelmann Turn, Upwind"," "," "," ","man1",5},
-{"45 Degree Upline, Upwind"," "," "," ","man1",5},
-{"Straight Inverted Flight, Downwind"," "," "," ","man1",5},
-{"Two Inside Loops, Upwind"," "," "," ","man1",5},
-{"Landing, Upwind"," "," "," ","man19",5},
+{"Straight Flight Out, Upwind"," "," "," ","man2",5},
+{"Procedure Turn, Turnaround"," "," "," ","man3",5},
+{"Straight Flight Back, Downwind"," "," "," ","man4",5},
+{"Stall Turn without Rolls, Upwind"," "," "," ","man5",5},
+{"Immelmann Turn, Upwind"," "," "," ","man6",5},
+{"45 Degree Upline, Upwind"," "," "," ","man7",5},
+{"Straight Inverted Flight, Downwind"," "," "," ","man8",5},
+{"Two Inside Loops, Upwind"," "," "," ","man9",5},
+{"Landing, Upwind"," "," "," ","man10",5},
 }
