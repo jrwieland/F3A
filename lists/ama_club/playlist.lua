4e8e995c8a20da1bf53172690f2e2073c3ea4d1b
John Wieland <john.r.wieland@gmail.com>
2021-04-04 20:43:30 -0400

    Call Sequences and Sounds Uploaded

---
+++ lists/ama_club/playlist.lua
@@ -0,0 +1,13 @@
+title="AMA Club Class Contest"
+playlist={
+{"Takeoff, Upwind"," "," "," ","man1",5},
+{"Straight Flight Out, Upwind"," "," "," ","man1",5},
+{"Procedure Turn, Turnaround"," "," "," ","man1",5},
+{"Straight Flight Back, Downwind"," "," "," ","man1",5},
+{"Stall Turn without Rolls, Upwind"," "," "," ","man1",5},
+{"Immelmann Turn, Upwind"," "," "," ","man1",5},
+{"45 Degree Upline, Upwind"," "," "," ","man1",5},
+{"Straight Inverted Flight, Downwind"," "," "," ","man1",5},
+{"Two Inside Loops, Upwind"," "," "," ","man1",5},
+{"Landing, Upwind"," "," "," ","man19",5},
+}
